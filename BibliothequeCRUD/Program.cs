64d93e44401c561934f962917b305f5f72b10307
Med <kottomouyema.m@gmail.com>
2026-03-10 15:03:59 +0100

    Add Book

--- BibliothequeCRUD/Program.cs
+++ BibliothequeCRUD/Program.cs
@@ -2,11 +2,7 @@
 {
     internal class Program
     {
-        static void RevenirAuMenuPrincipal(string messageMenuPrincipal)
-        {
-            Console.Clear();
-            AfficherMenu(messageMenuPrincipal);
-        }
+        
         static string DemanderChoixUtilisateurStr()
         {
             Console.Write("Tapez la touche Entrez pour revenir au menu principal : ");
@@ -27,38 +23,75 @@
 
         }
 
-        static void AfficherMenu(string message)
+        static void AjouterLivre(Dictionary<int, List<string>> bibliotheque, int compteurNbLivres)
         {
+            bool ajouterLivre = true;
+
+            while (ajouterLivre)
+            {
+                // Demander à l'utilisateur les informations nécessaires pour la création d'un livre
+
+                Console.Write("Saisir le titre : "); 
+                string titre = Console.ReadLine();
+
+                Console.Write("Saisir l'auteur : ");
+                string auteur = Console.ReadLine();
+
+                // Incrémenter le "compteur" à chaque livre crée       
+
+                compteurNbLivres++;
+
+                // Créer + ajouter le livre dans la bibliothèque 
+
+                bibliotheque.Add(compteurNbLivres, new List<string> { titre, auteur });
+
+                Console.WriteLine("Votre livre a été ajouté !"); 
+                Console.WriteLine();
+                Console.WriteLine("Voulez-vous ajouter un autre livre ? (o/n)");
+                string reponse = Console.ReadLine();  
+
+                if (reponse == "n")  
+                {
+                    return; 
+                }
+            }
+        } 
+       
+        static void GestionnaireDeLivres(string message, Dictionary<int,List<string>> bibliotheque)  
+        {
+            // Création d'un compteur pour garder en mémoire le nombre de livres dans la bibliothèque
+            int compteurNbLivres = bibliotheque.Count;
+
             Console.WriteLine(message); 
             Console.WriteLine();
-
             int reponseUtilisateur = DemanderChoixUtilisateurInt();
-            int optionSousMenu;
-            string optionQuitterSousMenu; 
+            string optionQuitter; 
             
             if (reponseUtilisateur == 1) 
             {
                 Console.Clear();
-                Console.WriteLine("Menu ajouter un livre"); 
+                AjouterLivre(bibliotheque, compteurNbLivres); 
                 Console.WriteLine();
-                optionQuitterSousMenu = DemanderChoixUtilisateurStr(); 
+                optionQuitter = DemanderChoixUtilisateurStr(); 
 
-                if (optionQuitterSousMenu == "")
+                if (optionQuitter == "")
                 {
-                    RevenirAuMenuPrincipal(message);  
-                }                            
+                    Console.Clear();
+                    GestionnaireDeLivres(message, bibliotheque);  
+                }      
 
             }
             else if (reponseUtilisateur == 2)
             {
                 Console.Clear();
-                Console.WriteLine("Menu afficher les livres"); 
-                Console.WriteLine();
-                optionQuitterSousMenu = DemanderChoixUtilisateurStr();
+                Console.WriteLine("Menu afficher livres"); 
+                Console.WriteLine(); 
+                optionQuitter = DemanderChoixUtilisateurStr();
 
-                if (optionQuitterSousMenu == "")
+                if (optionQuitter == "") 
                 {
-                    RevenirAuMenuPrincipal(message); 
+                    Console.Clear();
+                    GestionnaireDeLivres(message, bibliotheque); 
                 }
 
             }
@@ -67,11 +100,12 @@
                 Console.Clear();
                 Console.WriteLine("Menu modifier un livre");
                 Console.WriteLine();
-                optionQuitterSousMenu = DemanderChoixUtilisateurStr(); 
+                optionQuitter = DemanderChoixUtilisateurStr(); 
 
-                if (optionQuitterSousMenu == "")
+                if (optionQuitter == "")
                 {
-                    RevenirAuMenuPrincipal(message);
+                    Console.Clear();
+                    GestionnaireDeLivres(message, bibliotheque); 
                 }
 
             }
@@ -80,11 +114,12 @@
                 Console.Clear();
                 Console.WriteLine("Menu supprimer un livre");
                 Console.WriteLine();
-                optionQuitterSousMenu = DemanderChoixUtilisateurStr();
+                optionQuitter = DemanderChoixUtilisateurStr();
 
-                if (optionQuitterSousMenu == "")
+                if (optionQuitter == "")
                 {
-                    RevenirAuMenuPrincipal(message);
+                    Console.Clear();
+                    GestionnaireDeLivres(message, bibliotheque); 
                 }
 
             }
@@ -96,9 +131,12 @@
 
         static void Main(string[] args)
         {
-            string reponseUtilisateur = "";
+            /* On crée la bibliothèque (une seule fois) afin qu'elle reste accessible partout dans le programme
+            + permettre de pouvoir garder les informations en mémoire pour le CRUD ! */  
+            
+            Dictionary<int,List<string>> bibliotheque = new Dictionary<int,List<string>>();
 
-            AfficherMenu("-------- Menu -------- \n" +
+            string menuPrincipal = "-------- Menu -------- \n" +
                 "\n" +
                 "1. Ajouter un livre \n" +
                 "2. Afficher les livres\n" +
@@ -106,7 +144,9 @@
                 "4. Supprimer un livre\n" +
                 "5. Quitter\n" +
                 "\n" +
-                "----------------------");       
+                "----------------------";
+              
+            GestionnaireDeLivres(menuPrincipal, bibliotheque); 
             
         }
     }
