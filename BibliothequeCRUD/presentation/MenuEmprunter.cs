20d8720cba73ef2b9268a6a6f6ad7799cb031702
Med <kottomouyema.m@gmail.com>
2026-04-23 12:56:20 +0200

    Allow user to select a book

--- BibliothequeCRUD/presentation/MenuEmprunter.cs
+++ BibliothequeCRUD/presentation/MenuEmprunter.cs
@@ -30,9 +30,56 @@ namespace BibliothequeCRUD.presentation
             Console.WriteLine(". " + message); 
         }
 
+        public void AfficherLivreTrouve(Livre livreTrouve)
+        {
+            Console.WriteLine();
+            //Console.WriteLine("Livre n° : " + livreTrouve.id);
+            Console.WriteLine("Titre : " + livreTrouve.titre);
+            Console.WriteLine("Auteur : " + livreTrouve.auteur);
+            Console.WriteLine();
+        }
+
         public void EmprunterLivre()
         {
+            string reponseEmprunterLivre = ""; 
+
             menuAfficher.AfficherLivres();
+
+            // Vérifier que la bibliothèque possède au moins un livre avant d'emprunter un livre
+
+            bool livreExiste = gestionnaireLivres.LivreExiste();
+
+            if (!livreExiste)
+            {
+                return;
+            }
+            else
+            {
+                while (true)
+                {
+                    int idLivre = assistanceUtilisateur.DemanderIdLivre("Saisir l'identifiant du livre à emprunter : ");
+
+                    // Vérifier l'existance du livre 
+
+                    Livre livreARechercher = gestionnaireLivres.RechercherLivre(idLivre);
+
+                    // Si le livre trouvé -> afficher le livre, sinon -> afficher message erreur livre introuvable
+
+                    if (livreARechercher != null)
+                    {
+                        AfficherLivreTrouve(livreARechercher);
+                    }
+                    else // cas où on a pas trouvé le livre 
+                    {
+                        Console.WriteLine();
+                        assistanceUtilisateur.AfficherMessageErreurChoixUtilisateur("Erreur : livre introuvable ", ConsoleColor.Red);
+                        Console.WriteLine(); 
+                    }
+                }
+
+
+
+            }
         }
 
     }
